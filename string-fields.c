ab483f69f94899d6841a35d1cdfa1652d2f1240a
Aso Orji <42071241+asoorji@users.noreply.github.com>
2022-10-18 14:41:50 +0200

    Update string-fields.c

--- string-fields.c
+++ string-fields.c
@@ -2,7 +2,7 @@
 
 /**
  * get_precision - gets the precision from the format string
- * @p: the format string
+ * @p: the format strings
  * @params: the parameters struct
  * @ap: the argument pointer
  *
